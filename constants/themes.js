ef7bf047d916b7bc80975cd3b94a4a3e791653f5
Malek Mansour <malek24mansour@gmail.com>
2025-03-23 16:27:54 -0600

    Add themes.js

---
+++ constants/themes.js
@@ -0,0 +1,43 @@
+export const themes = {
+    dark: {
+      name: "Classic Dark Mode",
+      background: "#1a1a1a",
+      primary: "#182952",
+      secondary: "#213555",
+      text: "#F1F1F1",
+      delete: "#BF3131",
+    },
+    light: {
+      name: "Classic Light Mode",
+      background: "#F0F3FF",
+      primary: "#182952",
+      secondary: "#213555",
+      text: "#1a1a1a",
+      delete: "#BF3131",
+    },
+    astro: {
+      name: "Astro Neon",
+      background: "#211951",
+      primary: "#836FFF",
+      secondary: "#15F5BA",
+      text: "#F0F3FF",
+      delete: "#BF3131",
+    },
+    ocean: {
+      name: "Deep Blue Sea",
+      background: "#0B2447",
+      primary: "#19376D",
+      secondary: "#576CBC",
+      text: "#F0F3FF",
+      delete: "#BF3131",
+    },
+    cherry: {
+      name: "Cherry Blossom",
+      background: "#F0A8D0",
+      primary: "#F7B5CA",
+      secondary: "#F3D0D7",
+      text: "#F0F3FF",
+      delete: "#BF3131",
+    },
+  };
+  
