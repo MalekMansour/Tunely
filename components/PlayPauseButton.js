d6d900ee3e0ec7d2e9f61a19930d96dde31198c0
Malek Mansour <malek24mansour@gmail.com>
2025-03-23 17:52:12 -0600

    Update PlayPauseButton.js

--- components/PlayPauseButton.js
+++ components/PlayPauseButton.js
@@ -3,9 +3,11 @@ import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "../styles";
 import { useAudio } from "../context/AudioContext";
+import { useTheme } from "../context/ThemeContext";
 
 export default function PlayPauseButton({ song }) {
   const { playSound, pauseSound, resumeSound, currentSong, isPlaying } = useAudio();
+  const { theme } = useTheme(); 
 
   const handlePress = async () => {
     if (currentSong?.songId === song.songId) {
@@ -20,13 +22,13 @@ export default function PlayPauseButton({ song }) {
   };
 
   const isCurrentSong = currentSong?.songId === song.songId;
-  
+
   return (
     <TouchableOpacity onPress={handlePress} style={styles.iconButton}>
-      <Ionicons 
-        name={isCurrentSong && isPlaying ? "pause" : "play"} 
-        size={40} 
-        color="white"
+      <Ionicons
+        name={isCurrentSong && isPlaying ? "pause" : "play"}
+        size={40}
+        color={theme.text} 
       />
     </TouchableOpacity>
   );
