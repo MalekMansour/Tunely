8db394a5a2c34105be21891de1bd7b8b86b24c7c
Malek Mansour <malek24mansour@gmail.com>
2025-04-07 20:30:18 -0600

    Update TermsAndServices.js

--- screens/TermsAndServices.js
+++ screens/TermsAndServices.js
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F1F1F1', // Background color
+    paddingTop: 20, 
   },
   header: {
     flexDirection: 'row',
